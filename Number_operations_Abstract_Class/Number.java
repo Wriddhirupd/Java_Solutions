1c1bfc544e8abf3a2b70e9dae49ff8f3cdac08dc
Wriddhirup Dutta <48641152+Wriddhirupd@users.noreply.github.com>
2020-08-10 16:23:41 +0530

    Create Number.java

---
+++ Number_operations_Abstract_Class/Number.java
@@ -0,0 +1,14 @@
+package Numbers;
+
+public abstract class Number {
+	
+	 public abstract int sum(int a, int b, int c);
+	
+	 public abstract int average(int a, int b, int c);
+	
+	 public abstract int product(int a, int b, int c);
+	
+	 public abstract int smallest(int a, int b, int c);
+	
+	 public abstract int largest(int a, int b, int c);
+}
